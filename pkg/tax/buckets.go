02c1ad97b67955f70ae03164b04e1234c626f4b5
Jon Corbin <jon.corbin8@gmail.com>
2023-06-30 01:19:55 -0400

    remove validation

--- pkg/tax/buckets.go
+++ pkg/tax/buckets.go
@@ -7,16 +7,3 @@ const (
 	Roth        Bucket = "roth"
 	Taxable     Bucket = "taxable"
 )
-
-func IsValidTaxBucket(tb Bucket) bool {
-	switch tb {
-	case TaxDeferred:
-		return true
-	case Roth:
-		return true
-	case Taxable:
-		return true
-	default:
-		return false
-	}
-}
