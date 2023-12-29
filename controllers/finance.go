fe4ea03e1ba0068df08d7d0422367cb17e45b27c
Jon Corbin <jon.corbin8@gmail.com>
2023-12-29 00:31:20 -0500

    replace GetNetWorth with GetNetWorthOverTime

--- controllers/finance.go
+++ controllers/finance.go
@@ -1,8 +1,8 @@
 package controllers
 
 import (
-	"math"
 	"net/http"
+	"time"
 
 	"github.com/Jrc356/financial_dashboard/models"
 	"github.com/gin-gonic/gin"
@@ -15,33 +15,37 @@ type FinanceController struct {
 
 func NewFinanceController(db *gorm.DB, router *gin.RouterGroup) {
 	financeController := FinanceController{DB: db}
-	router.GET("/networth", financeController.CalculateNetWorth)
+	router.GET("/networth", financeController.GetNetWorthOverTime)
 }
 
-func CalculateCurrentTotalValue(db *gorm.DB, class models.AccountClass) (float64, error) {
-	accounts, err := models.GetAllAccountsByClass(db, class)
-	if err != nil {
-		return 0, err
-	}
-
-	var total float64
-	for _, asset := range accounts {
-		total += asset.Values[0].Value
-	}
-	return total, nil
-}
-
-func (fc *FinanceController) CalculateNetWorth(context *gin.Context) {
-	totalAssets, err := CalculateCurrentTotalValue(fc.DB, models.Asset)
+func (fc *FinanceController) GetNetWorthOverTime(context *gin.Context) {
+	assets, err := models.GetAllAccountsByClass(fc.DB, models.Asset)
 	if err != nil {
 		context.JSON(http.StatusInternalServerError, gin.H{"error": err.Error()})
 	}
-	totalLiabilities, err := CalculateCurrentTotalValue(fc.DB, models.Liability)
+	liabilities, err := models.GetAllAccountsByClass(fc.DB, models.Liability)
 	if err != nil {
 		context.JSON(http.StatusInternalServerError, gin.H{"error": err.Error()})
 	}
 
-	diff := totalAssets - totalLiabilities
-	rounded2d := math.Round(diff*float64(100)) / float64(100)
-	context.JSON(http.StatusOK, rounded2d)
+	values := make(map[time.Time]float64)
+
+	round := 5 * time.Second
+	for _, asset := range assets {
+		for _, v := range asset.Values {
+			entry := values[v.CreatedAt.Round(round)]
+			entry += v.Value
+			values[v.CreatedAt.Round(round)] = entry
+		}
+	}
+
+	for _, liability := range liabilities {
+		for _, v := range liability.Values {
+			entry := values[v.CreatedAt.Round(round)]
+			entry -= v.Value
+			values[v.CreatedAt.Round(round)] = entry
+		}
+	}
+
+	context.JSON(http.StatusOK, values)
 }
