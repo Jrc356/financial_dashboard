a7e1ab0a29b477dc03aba5ccf7867becbb55cb39
Jon Corbin <jon.corbin8@gmail.com>
2024-01-01 22:27:08 -0500

    rename test utils file; remove tags from vscode settings

--- server/models/utils_test.go
+++ server/models/utils_test.go
@@ -1,9 +1,8 @@
-//go:build test
-
 package models
 
 import (
 	"database/sql/driver"
+	"strings"
 	"time"
 
 	"github.com/DATA-DOG/go-sqlmock"
@@ -54,4 +53,34 @@ func AccountToSQLRow(account Account) *sqlmock.Rows {
 		time.Now(),
 		time.Now(),
 	)
+}
+
+type ExpectedStatement struct {
+	statement    string
+	args         []driver.Value
+	returnRows   *sqlmock.Rows
+	returnResult driver.Result
+	returnError  error
+}
+
+func LoadStatements(mock sqlmock.Sqlmock, statements []ExpectedStatement) {
+	for _, statement := range statements {
+		if strings.Contains(statement.statement, "SELECT") {
+			eq := mock.ExpectQuery(statement.statement).WithArgs(statement.args...)
+			if statement.returnRows != nil {
+				eq.WillReturnRows(statement.returnRows)
+			}
+			if statement.returnError != nil {
+				eq.WillReturnError(statement.returnError)
+			}
+		} else {
+			mock.ExpectBegin()
+			if statement.returnError != nil {
+				mock.ExpectRollback()
+			} else {
+				mock.ExpectExec(statement.statement).WithArgs(statement.args...).WillReturnResult(statement.returnResult)
+				mock.ExpectCommit()
+			}
+		}
+	}
 }
